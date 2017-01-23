ac4e3c5c27e49c18bdfb3305c9fd06606ab4a29e
Tobi <tobi@infinitag.io>
2017-01-23 23:35:56 +0100

    Strukturanpassungen

--- Tagger.ino
+++ Tagger.ino
@@ -15,7 +15,6 @@ const int muzzleLedPin = 8;
 const int displayResetPin = 4;
 const int displayDcPin = 5;
 const int displayCsPin = 6;
-const int lifePin = 13;
 bool alive = true;
 unsigned long timeOfDeath = 0;
 
@@ -31,24 +30,25 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, muzzleLedPin, NEO_GRBW + NEO_KHZ8
 
 void setup() {
   Serial.begin(57600);
-  Serial.println("booting");
   
+  Serial.println("booting Server...");
   SensorServer.initialize();
-  Serial.println("booting.");
+  
+  Serial.println("booting Events...");
   Wire.onRequest(requestEvent);
-  Serial.println("booting..");
 
+  Serial.println("booting Pins...");
   pinMode(fireBtnPin, INPUT);
-  pinMode(lifePin, OUTPUT);
   pinMode(muzzleLedPin, OUTPUT);
 
-  Serial.println("booting...");
+  Serial.println("booting Display...");
   SPI.begin();
   initialize_display(&display);
   
-  Serial.println("booting....");
+  Serial.println("booting LEDs...");
   strip.begin();
   colorWipe(strip.Color(0,0,0,0));
+  
   Serial.println("boot completed");
 }
 
@@ -56,36 +56,30 @@ void loop() {
   framebuffer.clear(BLACK);
   fireBtnState = digitalRead(fireBtnPin);
 
-  if(alive)
-  {
+  if (alive) {
     framebuffer.displayText("Alive", 50, 24, WHITE);
-    digitalWrite(lifePin, HIGH);
-    if(fireBtnState == HIGH)
-    {
+    colorWipe(strip.Color(0,255,0,0));
+    
+    if (fireBtnState == HIGH) {
       
       unsigned long shootValue = infinitagCore.ir_encode(false, 0, 2, 4, 1, 100);
       irsend.sendRC5(shootValue, 24);
-      colorWipe(strip.Color(0,255,0,0));
+      colorWipe(strip.Color(0,0,255,0));
       delay(100);
-      colorWipe(strip.Color(0,0,0,0));
+      colorWipe(strip.Color(0,255,0,0));
       //alive = false;
       //timeOfDeath = millis();
     }
-  }
-  else
-  {
-    digitalWrite(lifePin, LOW);
+  } else {
     unsigned long currentTime = millis();
     unsigned long ti = currentTime - timeOfDeath;
     char number[100];
     String(ti/1000).toCharArray(number, 100);
     framebuffer.displayText(number, 55, 24, WHITE);
-    if(ti > 500)
-    {
+    if (ti > 500) {
       colorWipe(strip.Color(0,0,0,0));
     }
-    if(ti > 20000) //20 seconds
-    {
+    if (ti > 20000) { //20 seconds
       alive = true; 
     }
   }
@@ -94,15 +88,14 @@ void loop() {
   delay(10);
 }
 
-void requestEvent()
-{
+void requestEvent() {
   //maybe we have enough time to look for the next free address?
   Serial.println("requested");
   SensorServer.registerNewClient();
 }
 
 void colorWipe(uint32_t c) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
+  for (uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
   }
   strip.show();
